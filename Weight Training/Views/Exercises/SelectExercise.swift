b405853bbf7915685876e12a525932d1da7f87fd
matt54633 <matt54633@gmail.com>
2023-11-02 16:53:39 +0000

    Add Existing Exercise from Exercise Form

---
+++ Weight Training/Views/Exercises/SelectExercise.swift
@@ -0,0 +1,18 @@
+//
+//  SelectExercise.swift
+//  Weight Training
+//
+//  Created by Matt Sullivan on 02/11/2023.
+//
+
+import SwiftUI
+
+struct SelectExercise: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectExercise()
+}
