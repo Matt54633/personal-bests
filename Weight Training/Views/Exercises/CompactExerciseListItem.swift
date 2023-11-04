326ccfc9d5d7fcc7814db02acbf3027d54099844
matt54633 <matt54633@gmail.com>
2023-11-04 16:43:34 +0000

    Bug fixes

---
+++ Weight Training/Views/Exercises/CompactExerciseListItem.swift
@@ -0,0 +1,18 @@
+//
+//  CompactExerciseListItem.swift
+//  Weight Training
+//
+//  Created by Matt Sullivan on 04/11/2023.
+//
+
+import SwiftUI
+
+struct CompactExerciseListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompactExerciseListItem()
+}
